46451c13d03460d0f3152df4babc0c8098698cdf
Ye Joo Park <subwaymatch@gmail.com>
2018-06-01 03:56:56 -0700

    Create 1_two_sum.java

---
+++ 1_two_sum.java
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+        vector<int> result;
+        int size = nums.size();
+
+        for (int i = 0; i < size; i++) {
+            for (int j = i + 1; j < size; j++) {
+                if (nums[i] + nums[j] == target) {
+                    result.push_back(i);
+                    result.push_back(j);
+
+                    return result;
+                }
+            }
+        }
+
+        return result;
+    }
+};
