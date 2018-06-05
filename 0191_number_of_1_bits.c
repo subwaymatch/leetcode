5ace7e2d1bee4ec5bab60ea2cc8dc5edab877fbc
Ye Joo Park <subwaymatch@gmail.com>
2018-06-04 18:59:28 -0700

    Update 0191_number_of_1_bits.c

--- 0191_number_of_1_bits.c
+++ 0191_number_of_1_bits.c
@@ -2,7 +2,7 @@ int hammingWeight(uint32_t n) {
     int weight = 0; 
     
     for (int i = 0; i < 32; i++) {
-        if (n & 1 == 1) weight++; 
+        weight += n & 1; 
         
         n = n >> 1; 
     }
