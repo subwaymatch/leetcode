4411f0718cc8f357f989948282e25deb7459e00b
Ye Joo Park <subwaymatch@gmail.com>
2018-06-09 05:48:23 -0700

    Update 0367_valid_perfect_square.java

--- 0367_valid_perfect_square.java
+++ 0367_valid_perfect_square.java
@@ -2,8 +2,6 @@ class Solution {
     public boolean isPerfectSquare(int num) {
         int maxRoot = (int) Math.sqrt(Integer.MAX_VALUE); 
         
-        System.out.println("maxRoot=" + maxRoot); 
-        
         for (int i = 1; i <= maxRoot; i++) {
             if (i * i == num) return true; 
             else if (i * i > num) break;  
