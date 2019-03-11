5d8c7b365980faca7ec88d5cfe4f04969f9e3117
Ye Joo Park <subwaymatch@gmail.com>
2019-03-11 07:25:30 +0100

    Create 0669_trim_a_binary_search_tree.py

---
+++ 0669_trim_a_binary_search_tree.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution:
+    def trimBST(self, root: TreeNode, L: int, R: int) -> TreeNode:
+        if root is None:
+            return None
+        
+        root.left = self.trimBST(root.left, L, R)
+        root.right = self.trimBST(root.right, L, R)
+        
+        if root.val < L:
+            root = root.right
+        elif root.val > R:
+            root = root.left
+            
+        return root
