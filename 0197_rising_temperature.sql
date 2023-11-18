3618cf65be5836fde12a24509f4bd178bc10eba7
Ye Joo Park <subwaymatch@gmail.com>
2023-11-17 23:04:20 -0600

    Create 0197_rising_temperature.sql

---
+++ 0197_rising_temperature.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT w2.id
+FROM Weather w1
+CROSS JOIN Weather w2
+WHERE
+  DATEDIFF(w2.recordDate, w1.recordDate) = 1 AND
+  w2.temperature > w1.temperature;
