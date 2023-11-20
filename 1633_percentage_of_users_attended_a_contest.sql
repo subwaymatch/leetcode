4c1a3ee83b63ac3367991111040f7165cd2b8bb7
Ye Joo Park <subwaymatch@gmail.com>
2023-11-19 23:20:45 -0600

    Create 1633_percentage_of_users_attended_a_contest.sql

---
+++ 1633_percentage_of_users_attended_a_contest.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT
+  r.contest_id,
+  IFNULL(ROUND(COUNT(u.user_id) / (SELECT COUNT(*) FROM Users) * 100, 2), 0) AS percentage
+FROM Register r
+LEFT JOIN Users u ON r.user_id = u.user_id
+GROUP BY r.contest_id
+ORDER BY percentage DESC, contest_id;
