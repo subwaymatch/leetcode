c0a03a0f54224ba0e32f7ce028fa6bec281ff624
Ye Joo Park <subwaymatch@gmail.com>
2023-12-19 19:23:22 -0600

    Create 1141_user_activity_for_the_past_30_days_i.sql

---
+++ 1141_user_activity_for_the_past_30_days_i.sql
@@ -0,0 +1,5 @@
+# slower soluton
+SELECT activity_date as day, COUNT(DISTINCT user_id) AS active_users
+FROM Activity
+WHERE (DATE(activity_date) BETWEEN DATE_SUB('2019-07-27', INTERVAL 29 DAY) AND '2019-07-27')
+GROUP BY activity_date;
