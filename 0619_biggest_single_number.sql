0409fa6942e313583758d533b7e26447ae24bc89
Ye Joo Park <subwaymatch@gmail.com>
2023-12-28 22:53:07 -0600

    Update 0619_biggest_single_number.sql

--- 0619_biggest_single_number.sql
+++ 0619_biggest_single_number.sql
@@ -1,9 +1,13 @@
-# Write your MySQL query statement below
+# MySQL SELECT Docs:
+# "You are permitted to specify DUAL as a dummy table name 
+# in situations where no tables are referenced"
+# FROM DUAL is optional
 SELECT (
     SELECT num
-    FROM my_numbers
+    FROM MyNumbers
     GROUP BY num
     HAVING COUNT(*) = 1
-    ORDER BY num desc
+    ORDER BY num DESC
     LIMIT 1
-) AS num;
+) AS num
+FROM DUAL;
