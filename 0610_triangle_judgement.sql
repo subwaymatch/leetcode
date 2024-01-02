60b383662854c1ee2fc63798432977ec04569b81
Ye Joo Park <subwaymatch@gmail.com>
2024-01-02 03:35:35 -0600

    Create 0610_triangle_judgement.sql

---
+++ 0610_triangle_judgement.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT
+    x, y, z,
+    IF(
+        x + y + z - GREATEST(x, y, z) > GREATEST(x, y, z),
+        "Yes",
+        "No"
+    ) AS triangle
+FROM Triangle;
